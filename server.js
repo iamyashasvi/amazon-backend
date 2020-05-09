4920076b113a843a819cb1cba17ef8bcbc54519a
G Yashasvi <g.yashasvi@mountblue.tech>
2020-05-09 18:19:55 +0530

    changed to config.port and model's index file takes value from config.js
    Also put immediate invoked function inside mysql so user can able change according to their environment.

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ require("dotenv").config({ path: "./.env" });
 const config = require("./config");
 const express = require("express");
 const app = express();
-const PORT = config.mysql.port;
+const PORT = config.PORT;
 
 let model = require("./src/models");
-console.log("config", config);
 model.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 });
--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,9 @@
-"use strict";
-
+require("dotenv").config({ path: "../../.env" });
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require("../../config");
 const db = {};
 
 let sequelize;
@@ -13,10 +11,13 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    config.mysql.database,
+    config.mysql.username,
+    config.mysql.password,
+    {
+      dialect: "mysql",
+      pool: config.mysql.pool,
+    }
   );
 }
 
