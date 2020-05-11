998b7e94cd4eb43166c360f22f9512c7fa1e1b92
G Yashasvi <g.yashasvi@mountblue.tech>
2020-05-11 12:50:45 +0530

    updated index.js file and removed config folder
    removed Sequelise from index.js and also removed env.use_env_variable because it is not usable currently.
    config folder contains configuration of model in json formate and i'm providing all the configuration from config.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,20 +6,15 @@ const basename = path.basename(__filename);
 const config = require("../../config");
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.mysql.database,
-    config.mysql.username,
-    config.mysql.password,
-    {
-      dialect: "mysql",
-      pool: config.mysql.pool,
-    }
-  );
-}
+let sequelize = new Sequelize(
+  config.mysql.database,
+  config.mysql.username,
+  config.mysql.password,
+  {
+    dialect: "mysql",
+    pool: config.mysql.pool,
+  }
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
@@ -39,6 +34,5 @@ Object.keys(db).forEach((modelName) => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
